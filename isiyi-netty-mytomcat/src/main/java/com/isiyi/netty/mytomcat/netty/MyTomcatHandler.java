f6688a0d1609fc4ab286115cdcb278bd475665d3
Ash-Shang <ash159@qq.com>
2021-03-01 14:41:49 +0800

    测试环境

--- isiyi-netty-mytomcat/src/main/java/com/isiyi/netty/mytomcat/netty/MyTomcatHandler.java
+++ isiyi-netty-mytomcat/src/main/java/com/isiyi/netty/mytomcat/netty/MyTomcatHandler.java
@@ -7,6 +7,7 @@ public class MyTomcatHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        // todo
         super.channelRead(ctx, msg);
     }
 }
